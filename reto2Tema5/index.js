40464f5f33b13d4ef996c6d6705df187080cae17
Cristina Taboada <cris.taboada98@gmail.com>
2023-03-13 16:07:39 +0100

    Modificada funcion suma para 3 operandos

--- reto2Tema5/index.js
+++ reto2Tema5/index.js
@@ -1,6 +1,6 @@
 // Funcion sumar
-function suma(op1, op2) {
-    return op1 + op2;
+function suma(op1, op2, op3) {
+    return op1 + op2 + op3;
 }
 
 // Funcion restar
--- reto2Tema5/run.js
+++ reto2Tema5/run.js
@@ -2,7 +2,7 @@
 let myLib = require("./index")
 
 // operaciones
-console.log(`Suma: ${myLib.suma(4,3)}`);
+console.log(`Suma: ${myLib.suma(4,3,1)}`);
 console.log(`Resta: ${myLib.resta(4,3)}`);
 console.log(`Producto: ${myLib.producto(4,3)}`);
 console.log(`Division: ${myLib.division(4,3)}`);
